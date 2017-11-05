356260b758012bd7e2986adef91bf0b4b8e2874b
novg <novg@localhost.localdomain>
2017-11-05 16:18:35 +0700

    base init.el

--- init.el
+++ init.el
@@ -1,134 +1,105 @@
 ;; M-x eval-buffer
 ;; M-x package-refresh-contents
-;; M-x package-install RET starter-kit RET
-(setq url-proxy-services '(("no_proxy" . "work\\.com")
-                           ("http" . "127.0.0.1:3128")
-                           ("https" . "127.0.0.1:3128")
-                           ))
+;; M-x package-install RET package-name RET
 
 (require 'package)
-
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 (when (not package-archive-contents)
-    (package-refresh-contents))
-;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (package-refresh-contents))
 
 ;; Set font
-(set-face-attribute 'default nil :font "Anonymous Pro Minus-13")
+;; (set-face-attribute 'default nil :font "Anonimous Pro Minus-13")
 
 ;; Start window size
-(when (window-system)
-    (set-frame-size (selected-frame) 145 42))
-;;установка левого верхнего угла фрейма
-(set-frame-position (selected-frame) 60 30)
+;; (when (window-system)
+;;    (set-frame-size (selected-frame) 122 34))
+;; Set top left corner of frame
+;; (set-frame-position (selected-frame) 60 30)
 
 ;; Disable GUI components
 (tooltip-mode      -1)
-(menu-bar-mode     -1) ;; отключаем графическое меню
-(tool-bar-mode     -1) ;; отключаем tool-bar
-(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
-(blink-cursor-mode -1) ;; курсор не мигает
-(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
-(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
-(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
-(global-set-key (kbd "<f12>") 'menu-bar-mode) ;; показать меню
+(menu-bar-mode     -1) ;; disable gui menu
+(tool-bar-mode     -1) ;; disable tool-bar
+(scroll-bar-mode   -1) ;; disable scroll-bar
+(blink-cursor-mode -1) ;; disable blink cursor
+(setq use-dialog-box nil) ;; disable gui dialog window
+(setq redisplay-dont-pause t) ;; advanced displaing of buffer
+(setq ring-bell-function 'ignore) ;; disable bell
+(global-set-key (kbd "<f12>") 'menu-bar-mode) ;; show menu
 
 ;; Inhibit startup/splash screen
 (setq inhibit-splash-screen   t)
-(setq ingibit-startup-message t) ;; экран приветствия можно вызвать комбинацией C-h C-a
+(setq inhibit-startup-message t) ;; show greeting screen C-h C-a
 
 ;; Display the name of the current buffer in the title bar
 (setq frame-title-format "GNU Emacs: %b")
 
-;; Возможность удалить выделенный текст при вводе поверх него
+;;
 (delete-selection-mode t)
 
-(size-indication-mode t) ;; размер файла в %-ах
+;; Size of file per percent (%)
+(size-indication-mode t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Формат даты и времени
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hour format
+;; Hour format
 (setq display-time-24hr-format t)
 ;; (setq display-time-day-and-date nil)
 (display-time)
 (setq calendar-date-display-form (quote ((format "%04s-%02d-%02d" year (string-to-int month) (string-to-int day)))))
 (setq calendar-time-display-form (quote (24-hours ":" minutes (if time-zone " (") time-zone (if time-zone ")"))))
-(setq calendar-week-start-day 1)
+(setq calendar-week-start-day t)
 (setq european-calendar-style t)
-;;
 
 ;; Linum plugin
-(require 'linum) ;; вызвать Linum
-(line-number-mode   t) ;; показать номер строки в mode-line
-(global-linum-mode  t) ;; показывать номера строк во всех буферах
-(column-number-mode t) ;; показать номер столбца в mode-line
-(setq linum-format " %d") ;; задаем формат нумерации строк
+(require 'linum)
+(line-number-mode   t) ;; show number of string in mode-line
+(global-linum-mode  t) ;; show nubers of strings at all buffers
+(column-number-mode t) ;; show number of column at mode-line
+(setq linum-format "  %d") ;; format of number strings
 
-;; Fringe settings
-(fringe-mode '(8 . 0)) ;; органичиталь текста только слева
-(setq-default indicate-empty-lines t) ;; отсутствие строки выделить глифами рядом с полосой с номером строки
-(setq-default indicate-buffer-boundaries 'left) ;; индикация только слева
+;; Fringe setting
+(fringe-mode '(8 . 0)) ;;
+(setq-default insicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
 
 ;; Syntax highlighting
 (require 'font-lock)
-(global-font-lock-mode             t) ;; включено с версии Emacs-22. На всякий...
 (setq font-lock-maximum-decoration t)
 
-;; Scrolling settings
-(setq scroll-step               1) ;; вверх-вниз по 1 строке
-(setq scroll-margin            10) ;; сдвигать буфер верх/вниз когда курсор в 10 шагах от верхней/нижней границы
+;; Scrolling setting
+(setq scroll-step               1)
+(setq scroll-margin            10)
 (setq scroll-conservatively 10000)
 
 ;; Show-paren-mode settings
-(show-paren-mode t) ;; включить выделение выражений между {},[],()
-(setq show-paren-style 'expression) ;; выделить цветом выражения между {},[],()
-
-;; Electric-modes settings
-(electric-pair-mode    1) ;; автозакрытие {},[],() с переводом курсора внутрь скобок
-;; (electric-indent-mode -1) ;; отключить индентацию  electric-indent-mod'ом (default in Emacs-24.4)
+(show-paren-mode t) ;; enable highlight expressions between {},[],()
+(setq show-paren-style 'expression) ;; enable color higlight expression between {},[],()
 
 ;; Disable backup/autosave files
-(setq make-backup-files        nil)
-(setq auto-save-default        t)
-;;(setq auto-save-list-file-name nil) ;;
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+;; (setq auto-save-list-file-name nil)
 
-;; Indent settings
-(setq-default indent-tabs-mode nil) ;; отключить возможность ставить отступы TAB'ом
-(setq-default tab-width          4) ;; ширина табуляции - 4 пробельных символа
+;; Indent setting
+(setq-default indent-tabs-mode nil) ;; disable TAB indent
+(setq-default tab-width          4) ;; widht of tabulation - 4 space symbols
 (setq-default c-basic-offset     4)
-(setq-default standart-indent    4) ;; стандартная ширина отступа - 4 пробельных символа
-(setq-default lisp-body-indent   4) ;; сдвигать Lisp-выражения на 4 пробельных символа
-;(global-set-key (kbd "RET") 'newline-and-indent) ;; при нажатии Enter перевести каретку и сделать отступ
-(setq lisp-indent-function  'common-lisp-indent-function)
+(setq-default standart-indent    4)
+(setq-default lisp-body-indent   4)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(setq lisp-indent-function 'common-lisp-indent-function)
 
-;; Short messages
+;; Show messages
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Clipboard settings
 ;; (setq x-select-enable-clipboard t)
 
 ;; End of file newlines
-(setq require-final-newline    t) ;; добавить новую пустую строку в конец файла при сохранении
-(setq next-line-add-newlines nil) ;; не добавлять новую строку в конец при смещении курсора  стрелками
+(setq require-final-newline    t)
+(setq next-line-add-newlines nil)
 
 ;; Easy transition between buffers: M-arrow-keys
 (if (equal nil (equal major-mode 'org-mode))
@@ -139,15 +110,10 @@
 (ido-mode                      t)
 (icomplete-mode                t)
 (ido-everywhere                t)
-(setq ido-vitrual-buffers      t)
+(setq ido-virtual-buffers      t)
 (setq ido-enable-flex-matching t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Назначение клавиш
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;C-w удаляет предыдущее слово или вырезает регион, если он выделен:
+;; C-w dalete previous word or region
 (defun backward-kill-word-or-kill-region (arg)
     (interactive "p")
     (if (region-active-p)
@@ -155,48 +121,42 @@
                      (region-end))
         (backward-kill-word arg)))
 (global-set-key (kbd "C-w") 'backward-kill-word-or-kill-region)
-
 (define-key minibuffer-local-map (kbd "C-w") 'backward-kill-word-or-kill-region)
-
 (add-hook 'ido-setup-hook
           (lambda ()
               (define-key ido-completion-map (kbd "C-w") 'ido-delete-backward-word-updir)))
 
-;; Buffer Selection and ibuffer settings
+;; Buffer selection and ibuffer settings
 (require 'bs)
 (require 'ibuffer)
-(defalias 'list-buffers 'ibuffer) ;; отдельный список буферов при нажатии C-x C-b
-(global-set-key (kbd "<f2>") 'bs-show) ;; запуск buffer selection кнопкой F2
+(defalias 'list-buffers 'ibuffer) ;; list of buffers on C-x C-b
+(global-set-key (kbd "<f2>") 'bs-show) ;; buffer selection on F2
 
-;; Easy transition between buffers: M-arrow-keys
-(if (equal nil (equal major-mode 'org-mode))
-    (windmove-default-keybindings 'meta))
-
-;; Bookmark settings
+;; Bookmarks settings
 (require 'bookmark)
-(setq bookmark-save-flag t) ;; автоматически сохранять закладки в файл
+(setq bookmark-save-flag t) ;; auto-save bookmarks in file
 (when (file-exists-p (concat user-emacs-directory "bookmarks"))
-    (bookmark-load bookmark-default-file t)) ;; попытаться найти и открыть файл с закладками
-(setq bookmark-default-file (concat user-emacs-directory "bookmarks")) ;; хранить закладки в файл bookmarks в .emacs.d
-(global-set-key (kbd "<f5>") 'bookmark-jump) ;; прыгнуть на закладку по F4
-(global-set-key (kbd "<C-f5>") 'bookmark-set) ;; создать закладку
-(global-set-key (kbd "<M-f5>") 'bookmark-bmenu-list) ;; открыть список закладок
+    (bookmark-load bookmark-default-file t))
+(setq bookmark-default-file (concat user-emacs-directory "bookmarks"))
+(global-set-key (kbd "<f5>") 'bookmark-jump) ;; jump on bookmark on F5
+(global-set-key (kbd "<C-f5>") 'bookmark-set) ;; create bookmark
+(global-set-key (kbd "<M-f5>") 'bookmark-bmenu-list) ;; open list of bookmarks
 
 ;; Dired
 (require 'dired)
-(setq dired-recursive-deletes 'top) ;; чтобы можно было непустые директории удалять...
+(setq dired-recursive-deletes 'top) ;; delete not empty directories
 
 ;; Imenu
 (require 'imenu)
-(setq imenu-auto-rescan      t) ;; автоматически обновлять список функций в буфере
-(setq imenu-use-popup-menu nil) ;; диалоги Imenu только в минибуфере
-(global-set-key (kbd "<f8>") 'imenu) ;; вызов Imenu на F8
+(setq imenu-auto-rescan t)
+(setq imeny-use-popup-menu nil)
+(global-set-key (kbd "<f8>") 'imenu)
 
 ;; Run shell
 (global-set-key (kbd "C-x /") 'shell-command)
 
-;; Режим word-wrap
-(global-set-key [f6] 'toggle-truncate-lines)
+;; Word-wrap mode
+(global-set-key (kbd "<f6>") 'toggle-truncate-lines)
 
 ;; Comment function
 (defun comment-or-uncomment-this (&optional lines)
@@ -204,23 +164,28 @@
     (if mark-active
         (if (< (mark) (point))
             (comment-or-uncomment-region (mark) (point))
-            (comment-or-uncomment-region (point) (mark)))
+            (comment-or-uncomment-region (poin) (mark)))
         (comment-or-uncomment-region
          (line-beginning-position)
          (line-end-position lines))
         (next-line)))
 (global-set-key (kbd "C-;") 'comment-or-uncomment-this)
 
-;;C-x C-k убивает буфер:
+;; C-x C-k killing buffer
 (defun kill-current-buffer ()
     (interactive)
     (kill-buffer (current-buffer)))
 (global-set-key (kbd "C-x C-k") 'kill-current-buffer)
 
-;;S-Space и M-/ используются для умного автодополнения:
-;; (global-set-key (kbd "M-/") 'hippie-expand)
-;; (global-set-key (kbd "S-SPC") 'dabbrev-expand)
-(global-hl-line-mode 1) ;; подсвечивать текущую строку
+;; Auto-complete
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "S-SPC") 'dabbrev-expand)
 
+;; Highlight current line
+(global-hl-line-mode t)
+
+;;
+;;
+;;
 (add-to-list 'load-path "~/.emacs.d/elisp/")
-(require 'init-haskell)
+; (require 'init-haskell)
